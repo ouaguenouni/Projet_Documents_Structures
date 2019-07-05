f55e0747db85e4b836527e693dd6a0899a1ce3de
MoohShadox <ouaguenouni.hachemi@gmail.com>
2019-07-05 15:12:06 +0100

    Ajout du programme de transformation du fichier Renault

---
+++ src/Transformateur_Renault.java
@@ -0,0 +1,86 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Transformateur_Renault {
+
+    public static void parcourRecursif(Node E, StringBuilder ss) {
+
+        NodeList LN = E.getChildNodes();
+        for (int i = 0; i < LN.getLength(); i++) {
+            if (LN.item(i).getNodeValue() != null && !LN.item(i).getNodeValue().equals("null"))
+                ss.append(LN.item(i).getNodeValue());
+            if (E.hasChildNodes())
+                parcourRecursif(LN.item(i), ss);
+        }
+    }
+
+    public static void main(String[] str) {
+
+
+    }
+
+    public Element createElement(Document Doc) {
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        final DocumentBuilder builder;
+        final Document document;
+        final Element racine2 = Doc.createElement("Concessionaires");
+        Doc.appendChild(racine2);
+        try {
+
+            builder = factory.newDocumentBuilder();
+            document = builder.parse(new File("C:\\Users\\Geekzone\\Desktop\\Projet_Documents_Structures\\examen\\examen_bis\\poeme\\fiches\\renault\\renault.html"));
+            final Element racine = document.getDocumentElement();
+
+            StringBuilder ch = new StringBuilder("");
+            parcourRecursif(racine, ch);
+            String chaine = ch.toString();
+            Pattern pattern = Pattern.compile("(\\s)+");
+            Pattern pattern2 = Pattern.compile("Succursale .* end post");
+            Matcher matcher = pattern.matcher(chaine);
+
+
+            // check all occurance
+            String retour = matcher.replaceAll(" ");
+            Matcher matcher2 = pattern2.matcher(retour);
+            System.out.println("Chaine traitée :");
+
+            matcher2.find();
+            String nch = matcher2.group();
+            System.out.println(nch);
+            Pattern pattern3 = Pattern.compile("(([A-Za-z]|\\s)*?) Adresse( )?: ((.*?)) Tél( )?: ((\\d+ \\d+ \\d+ \\d+)) (Fax :( )*\\d+ \\d+ \\d+ \\d+)*");
+            Matcher matcher3 = pattern3.matcher(nch);
+            while (matcher3.find()) {
+                System.out.println("MATCHE : ");
+                System.out.println("Total : " + matcher3.group());
+                System.out.println("Nom : " + matcher3.group(1));
+                Element D1 = Doc.createElement("Nom");
+                D1.appendChild(Doc.createTextNode(matcher3.group(1)));
+                racine2.appendChild(D1);
+                System.out.println("Adresse : " + matcher3.group(4));
+                Element D2 = Doc.createElement("Adresse");
+                D2.appendChild(Doc.createTextNode(matcher3.group(4)));
+                racine2.appendChild(D2);
+                System.out.println("Téléphone : " + matcher3.group(7));
+                Element D3 = Doc.createElement("Num_téléphone");
+                D3.appendChild(Doc.createTextNode(matcher3.group(7)));
+                racine2.appendChild(D3);
+            }
+
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+        }
+        return racine2;
+    }
+
+}
